66a97ea8f809edadc995a6ec00df211cd6685864
ivandbs2000 <ivandbs2000@javerianacali.edu.co>
2018-09-27 16:54:15 -0500

    Add files via upload

---
+++ Revista/conexion.php
@@ -0,0 +1,7 @@
+<?php
+	$mysqli = new mysqli('localhost', 'root', '', 'mi_revista');
+	$mysqli -> set_charset("utf8");
+	$queryCiudad = $mysqli -> query ("SELECT * FROM ciudad");
+	$queryRevista = $mysqli -> query ("SELECT * FROM revista");
+
+?>
---
+++ Revista/src/subject.js
@@ -0,0 +1,14 @@
+function searchTupla(){
+
+	var search = document.getElementById('search').value;
+	var dataEn = 'search='+search;
+}
+
+function x(){
+	var identification = document.getElementById('identification').value;
+	var fisrt_name = document.getElementById('search').value;
+}	
+
+function xd(){
+	
+}
